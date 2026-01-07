fad7a9c7ac6108df3b92a4a0795ee29e3f88296b
priyadharshinidb26-sudo <priyadharshinidb26@gmail.com>
2026-01-07 12:03:49 +0530

    Create Style.css

---
+++ Style.css
@@ -0,0 +1,24 @@
+body {
+    text-align: center;
+    font-family: Arial;
+    background-color: #f2f2f2;
+}
+
+input {
+    padding: 10px;
+    margin: 5px;
+}
+
+button {
+    padding: 10px;
+    background-color: green;
+    color: white;
+    border: none;
+}
+
+li {
+    background: white;
+    margin: 5px;
+    padding: 10px;
+    list-style: none;
+}
